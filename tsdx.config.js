8821c8bd90ebbfb5a182c594a3e277f84ec82c8e
Vincent <0426vincent@gmail.com>
2023-07-18 17:05:20 +0800

    build

--- tsdx.config.js
+++ tsdx.config.js
@@ -16,15 +16,15 @@ module.exports = {
         ],
         // only write out CSS for the first bundle (avoids redundant extra files)
         // user need to include the css file in the build folder
-        // extract: !!options.writeMeta,
-        // inject: false,
+        extract: !!options.writeMeta,
+        inject: false,
         
         // No need to include css file in the build folder,
         // since we are using css-in-js
         // Append to <head /> as code running
-        inject: true,
+        // inject: true,
         // Keep it as false since we don't extract to css file anymore
-        extract: false,
+        // extract: false,
       })
     );
     config.plugins.push(typescript(
